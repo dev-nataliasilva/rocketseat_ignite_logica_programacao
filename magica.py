980647c1af60cc8eae35f766a7d0d33a1392b67b
Natalia Silva <natalia.nsdeveloper@gmail.com>
2025-06-11 22:04:55 -0300

    aula 2

---
+++ magica.py
@@ -0,0 +1,16 @@
+import os
+
+os.system('cls')
+
+print ('Olhe as cartas abaixo e escolha uma:')
+print ('------')
+print('Q♣️  K♦️  J♥️  Q♥️  J♣️  K♠️')
+input()
+
+os.system('cls')
+print('A carta que você pensou...')
+input()
+
+print ('Desapareceu!')
+print('J♠️  Q♦️  J♦️  Q♠️  K♥️')
+input()
