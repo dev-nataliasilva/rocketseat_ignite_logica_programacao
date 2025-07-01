dbefdc6ed61abd8261d6f56b3c8410fd9273b901
Natalia Silva <natalia.nsdeveloper@gmail.com>
2025-07-01 16:17:36 -0300

    variaveis e madlib

--- programa.py
+++ programa.py
@@ -1,2 +1,14 @@
 print("Hello, world!")
 print(1 + 1)
+
+#Tipos de dados Python
+# numéricos, texto, booleano (lógico)
+# estaticamente tipada -> declarar tipos antes de usar
+# dinamicamente tipada -> não precisa declarar
+
+# fortemente tipada -> python
+# fracamente tipada -> js (deixa misturar)
+
+#python terminal interativo
+
+#int, float, complex
